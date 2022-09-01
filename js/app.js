7bec48971ab432e278a653d3fb66d6121b16e88f
adoublef <kristopherab@gmail.com>
2022-09-01 16:50:51 +0100

    waiting for review

--- js/app.js
+++ js/app.js
@@ -1,62 +1,77 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+const sections = document.querySelectorAll("section");
+const ul = document.getElementById("navbar__list");
 
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+const VALUE = 150;
 
 // build the nav
 
+ul.append(...[...sections].map(createLink));
+
+// add event listeners
+
+document.addEventListener("scroll", makeActive);
+document.addEventListener("click", onScroll);
+
+// Scroll to section
+
+function onScroll(e) {
+    e.preventDefault();
+
+    const { target: el } = e;
+
+    el.tagName.toLowerCase() === "a" &&
+        document
+            .getElementById(el.dataset.section)
+            .scrollIntoView({ behavior: "smooth" });
+}
 
 // Add class 'active' to section when near top of viewport
 
+function makeActive() {
+    for (const section of sections) {
+        const box = section.getBoundingClientRect();
+        const a = ul.querySelector(`[data-section="${section.id}"]`);
 
-// Scroll to anchor ID using scrollTO event
+        //Find a value that works best, but 150 seems to be a good start.
+        if (box.top <= VALUE && box.bottom >= VALUE) {
+            //apply active state on current section and corresponding Nav link
+            section.classList.add("active--section");
+            a.classList.add("active--link");
 
+        } else {
+            //Remove active state from other section and corresponding Nav link
+            section.classList.remove("active--section");
+            a.classList.remove("active--link");
+        }
+    }
+}
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+// Create nav link that will be appended to the nav list
 
-// Build menu 
+function createLink(section, i) {
+    const [a, li] = [document.createElement("a"), document.createElement("li")];
 
-// Scroll to section on link click
+    a.classList.add("menu__link");
+    a.innerHTML = section.dataset.nav;
+    a.dataset.section = section.id;
+    a.dataset.position = `${++i}`;
 
-// Set sections as active
+    li.appendChild(a);
 
-
+    return li;
+};
